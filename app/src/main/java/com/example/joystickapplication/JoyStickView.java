59fe9facbab6859421e1bfd41930c1e4b1e0bd50
Ariel Alexi <ariel.147@gmail.com>
2019-06-13 18:59:25 +0300

    creating the joystick view starting the model

--- app/src/main/java/com/example/joystickapplication/JoyStickView.java
+++ app/src/main/java/com/example/joystickapplication/JoyStickView.java
@@ -5,85 +5,78 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
-import android.util.AttributeSet;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView; // the base of the joystick that we will build upon
+import android.util.DisplayMetrics;
+import android.view.View;
 
-import java.text.AttributedCharacterIterator;
 
-public class JoyStickView extends SurfaceView {
+public class JoyStickView extends View {
 
-    public static float centerX;
-    public static float centerY;
-    public static float baseRadius;
-    public static float hatRadius;
+    public float centerX;
+    public float centerY;
+
+    public float baseRadius;
+    public float hatRadius;
+
+    public float getHatRadius() {
+        return hatRadius;
+    }
+
+    public float newX;
+    public float newY;
+
+    public void setNewX(float newX) {
+        this.newX = newX;
+    }
+
+    public void setNewY(float newY) {
+        this.newY = newY;
+    }
+
+    public float getCenterX() {
+        return centerX;
+    }
+
+    public float getCenterY() {
+        return centerY;
+    }
 
     public JoyStickView(Context context) {
         super(context);
-    }
 
-    public JoyStickView(Context context, AttributeSet attributes, int style) {
-        super(context, attributes, style);
-    }
+        DisplayMetrics dm = getResources().getDisplayMetrics();
 
-    public JoyStickView(Context context, AttributeSet attributes) {
-        super(context, attributes);
-    }
-
-    // the function assign the variables their values
-    public void setupDimensions() {
-        centerX = getWidth() / 2; // the width of the surfaceview
-        centerY = getHeight() / 2; // the height of the surfaceview
+        newX = centerX = (float) dm.widthPixels / 2; // the width
+        newY = centerY = (float) dm.heightPixels / 2; // the height
         // ensures that the joystick will always fit inside the surface view even if one dimension is greatly smaller than the other.
-        baseRadius = Math.min(getWidth(), getHeight()) / 3;
-        hatRadius = Math.min(getWidth(), getHeight()) / 5;
+        baseRadius = (float) Math.min(dm.widthPixels, dm.heightPixels) / 3;
+        hatRadius = (float) Math.min(dm.widthPixels, dm.heightPixels) / 6;
+
     }
 
-    //now we will implement the callbacks.
-    // callback = a way that an object within a class can notify the class that a specific event has occurred within it
 
+    // todo : create view when flipping the screen
 
-    //this method will be called when the SurfaceView has been fully created and has all the dimensions laid out and is ready for drawing.
-
-
-
-    //@Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        //set the callback methods in this class to be the ones to be called when those events happen.
-        setupDimensions(); // init the joystick position
-        drawJoystick(centerX, centerY);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawJoystick(canvas, this.newX, this.newY);
     }
 
-    //@Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        //set the callback methods in this class to be the ones to be called when those events happen.
-    }
 
-    //@Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        //set the callback methods in this class to be the ones to be called when those events happen.
-    }
-
+    //todo : NICE TO HAVE : to change color ASAP !
+    // todo : MUST : to fix the position of the big and small circles on the screen !!!
 
     // drawing the joystick on the screen
-    // params: newX, newY - used to specify the location of the hat (the yop of the joystick) on the SurfaceView
-    private void drawJoystick(float newX, float newY) {
+    private void drawJoystick(Canvas myCanvas, float newX, float newY) {
+        Paint colors = new Paint();
+        myCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR); // Clearing the BG
 
-        // preventing the drawing from executing when the surfaceview has not been created on-screen , preventing exceptions at runtime
-        if (getHolder().getSurface().isValid()) {
+        // todo : NICE TO HAVE : to move the coloring to constant params
 
-            Canvas myCanvas = this.getHolder().lockCanvas(); // stuff to draw
-            Paint colors = new Paint();
-            myCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR); // Clearing the BG
-            colors.setARGB(25, 50, 50, 50);// color of the joystick base
-            myCanvas.drawCircle(centerX, centerY, baseRadius, colors); // draw the joystick base
-            colors.setARGB(255, 0, 0, 255); // colo of the joystick itself
-            myCanvas.drawCircle(newX, newY, hatRadius, colors); // draw the joystick hat
-            getHolder().unlockCanvasAndPost(myCanvas); // write the new drawing to the SurfaceView
-
-        }
+        colors.setColor(Color.argb(250, 50, 50, 50));// color of the joystick base
+        myCanvas.drawCircle(centerX, centerY, baseRadius, colors); // draw the joystick base
+        colors.setColor(Color.argb(255, 0, 0, 255));// colo of the joystick itself
+        myCanvas.drawCircle(newX, newY, hatRadius, colors); // draw the joystick hat
 
     }
-
-
 }
--- app/src/main/java/com/example/joystickapplication/JoystickActivity.java
+++ app/src/main/java/com/example/joystickapplication/JoystickActivity.java
@@ -2,16 +2,52 @@ package com.example.joystickapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.widget.Button;
 
 public class JoystickActivity extends AppCompatActivity {
+
+    private JoyStickView joyStickView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        JoyStickView joyStickView = new JoyStickView(this);
+        this.joyStickView = new JoyStickView(this);
         setContentView(joyStickView);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                this.joyStickView.setNewX(joyStickView.getCenterX());
+                this.joyStickView.setNewY(joyStickView.getCenterY());
+                break;
+
+            case MotionEvent.ACTION_DOWN:
+
+                if (Math.sqrt(Math.pow(this.joyStickView.getCenterX() - event.getX(), 2) +
+                        Math.pow(this.joyStickView.getCenterY() - event.getY(), 2))
+                        > this.joyStickView.getHatRadius())
+                    return false;
+                else
+
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                this.joyStickView.setNewX(event.getX());
+                this.joyStickView.setNewY(event.getY());
+                break;
+
+        }
+
+        this.joyStickView.postInvalidate(); // "recall" the view draw again
+        return true;
 
     }
 }
